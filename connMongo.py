b9e933e333f2b6f0d663a323e487565943a04425
TomasAQ <tomasaq0511@gmail.com>
2021-01-05 15:47:55 +0900

    insert data in mongodb

---
+++ connMongo.py
@@ -0,0 +1,51 @@
+import pymongo
+from pymongo import MongoClient
+import pandas as pd
+
+# mongodb 접속
+def mongoConn() :
+    global collect;
+    host = "localhost"
+    port = 27017
+    conn =MongoClient(host, port)
+    #weater 라는 이름의 데이터베이스가 만들어짐
+    db=conn.weather
+    #weater에 cheonYeondong 컬렉션 생성
+    collect = db.cheonYeondong
+
+# csv 파일 읽고 안에 있는 데이터 넣기
+def mongoInsertData() :
+    dfChen_Y = pd.read_csv("cheon_yeondong.csv")
+
+    for i in  range(len(dfChen_Y)):
+        # 컬럼 명
+        #a, day, hour, precipitation_form, humidity, precipitation, temperature, date = dfChen_Y.loc[[i]]
+        # 테이터
+        a, dayVal, hourVal, precipitation_formVal, humidityVal, precipitationVal, temperatureVal, dateVal = dfChen_Y.loc[i]
+
+        collect.insert_one({'date': str(dateVal) , 'day' : str(dayVal) , 'hour' : str(hourVal) , 'precipitationForm' : float(precipitation_formVal) , 'humidity' : float(humidityVal) , 'precipitation' : float(precipitationVal) , 'temperature' : float(temperatureVal) })
+# collection 전체 제거
+def delectAllCollect() :
+    # 전체 문서 삭제
+    collect.drop()
+    # 항목 하나 삭제
+    #collect.delete_one({'202001' : {'format: day' : 1} })
+
+
+# collection 내용 변경
+def updateEx() :
+    #collect.update_one({'no': 1}, {'$set': {'kor': 100, 'eng': 100}})
+    pass
+
+# 모든 데이터 출려
+def showData() :
+    rows = collect.find()
+    for row in rows :
+        print(row)
+
+if __name__ == '__main__':
+    mongoConn()
+    # 초기에 한번만 실행
+    #mongoInsertData()
+    showData()
+
--- crud_csv.py
+++ crud_csv.py
@@ -25,9 +25,38 @@ def get_csv_data():
     dfRes['precipitation'] = dfPre['precipitation']
     dfRes['temperature'] = dfTem['temperature']
 
-    # 결과
-    #print(dfRes)
-    #print(dfRes.columns)
+
+    # 결손값 제거
+    dfRes = dfRes.dropna()
+
+    # 새로운 로직 필요
+    # date + 년월 추가해서 새로운 컬럼 생성
+    dfRes['date'] = dfRes[' format: day']
+    dfRes[:744]['date'] = '202001' + dfRes[' format: day']
+    dfRes[744:1440]['date'] = '202002' + dfRes[' format: day']
+    dfRes[1440:2184]['date'] = '202003' + dfRes[' format: day']
+    dfRes[2184:2904]['date'] = '202004' + dfRes[' format: day']
+    dfRes[2904:3648]['date'] = '202005' + dfRes[' format: day']
+    dfRes[3648:4368]['date'] = '202006' + dfRes[' format: day']
+    dfRes[4368:5112]['date'] = '202007' + dfRes[' format: day']
+    dfRes[5112:5856]['date'] = '202008' + dfRes[' format: day']
+    dfRes[5856:6576]['date'] = '202009' + dfRes[' format: day']
+    dfRes[6576:7320]['date'] = '202010' + dfRes[' format: day']
+    dfRes[7320:8040]['date'] = '202011' + dfRes[' format: day']
+    dfRes[8040:]['date'] = '202012' + dfRes[' format: day']
+
+
+    #  조건 2개를 통한 값 불려오기
+    # for01 = dfRes[' format: day'] == ' 1'
+    # for02 = dfRes[' format: day'] == ' 2'
+    # print(dfRes[for01 | for02].head(30))
+
+    #csv 파일 만들기
+    makeCsv(dfRes)
+# csv파일 만들기
+def makeCsv(dfRes) :
+    dfRes = dfRes
+    # dfRes.to_csv("cheon_yeondong_nan.csv")
     dfRes.to_csv("cheon_yeondong.csv")
 
 
